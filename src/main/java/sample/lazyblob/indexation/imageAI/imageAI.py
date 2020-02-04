1ee0f03401c39d13da3251b7d339dba0412e32fe
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-02-04 14:24:30 +0100

    Update script with argument

--- src/main/java/sample/lazyblob/indexation/imageAI/imageAI.py
+++ src/main/java/sample/lazyblob/indexation/imageAI/imageAI.py
@@ -1,4 +1,4 @@
-#from imageai.Detection import ObjectDetection
+from imageai.Detection import ObjectDetection
 import os
 import sys
 import numpy as np
@@ -6,11 +6,10 @@ import numpy as np
 if len(sys.argv) != 2:
     raise Exception('Invalid number of arguments. 1 argument is required.')
 
-
 execution_path = os.getcwd()
 im_name = str(sys.argv[1])
 nn_name = 'resnet50_coco_best_v2.0.1.h5'
-threshold = 75
+threshold = 70
 
 
 detector = ObjectDetection()
@@ -20,5 +19,5 @@ detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , im_name), output_image_path=os.path.join(execution_path , "output.jpg") , minimum_percentage_probability=threshold)
 
 for eachObject in detections:
-    # print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+    #print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
     print(eachObject["name"])
