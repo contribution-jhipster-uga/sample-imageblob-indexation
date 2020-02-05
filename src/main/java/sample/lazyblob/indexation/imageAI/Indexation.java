32045fc5709123b8a489c4a0c93d1965fe920b21
Tom SOLVERY <solverytom@hotmail.fr>
2020-02-05 13:58:53 +0100

    Ajout ImageIA detection

--- src/main/java/sample/lazyblob/indexation/imageAI/Indexation.java
+++ src/main/java/sample/lazyblob/indexation/imageAI/Indexation.java
@@ -3,28 +3,83 @@ package sample.lazyblob.indexation.imageAI;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.ocr.TesseractOCRParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Indexation {
-    public static String parseMetadata(String filename){
+    public static String parseMetadata(String filename) {
         AutoDetectParser parser = new AutoDetectParser();
         BodyContentHandler handler = new BodyContentHandler();
         Metadata metadata = new Metadata();
         String result = new String();
-        try{
+        try {
             InputStream stream = new BufferedInputStream(new FileInputStream(filename));
-            parser.parse(stream,handler,metadata);
+            parser.parse(stream, handler, metadata);
             result = metadata.toString();
             System.out.println(result);
         } catch (IOException | SAXException | TikaException e) {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public static String parseTextFromImage(String filename) {
+        TesseractOCRParser parser = new TesseractOCRParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
+        String result = "";
+        try {
+            InputStream stream = new BufferedInputStream(new FileInputStream(filename));
+            parser.parse(stream, handler, metadata, context);
+            result = handler.toString();
+            System.out.println(result);
+        } catch (SAXException | TikaException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    public static String imageAI(String pathImg) throws Exception {
+
+        String script = "src/imageAI.py ";
+        String commandToExecute = "python3 " + script + pathImg;
+        String res = "";
+        int error = -1;
+
+        try {
+            System.out.println(commandToExecute);
+
+            Process pr = Runtime.getRuntime().exec(commandToExecute);
+            error = pr.waitFor();
+            InputStream stdout = pr.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
+            String line;
+
+            try {
+                while ((line = reader.readLine()) != null) {
+                    if(!line.contains("tracking")) {
+                        res = res + line + " ";
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Exception in reading output" + e.toString());
+            }
+            System.out.println(error);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(error != 0) {
+            throw new Exception("Error during script execution.");
+        }
+
+        return res;
     }
 }
